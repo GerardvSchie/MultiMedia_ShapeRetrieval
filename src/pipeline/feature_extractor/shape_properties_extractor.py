5b49c580f4089e6b36afcd724d12e9fdde3ab3a7
shreyashm20 <shreyashm20@gmail.com>
2022-10-26 19:40:39 +0200

    Added histograms

--- src/pipeline/feature_extractor/shape_properties_extractor.py
+++ src/pipeline/feature_extractor/shape_properties_extractor.py
@@ -1,6 +1,6 @@
 import open3d as o3d
 import numpy as np
-import random
+
 
 from src.controller.geometries_controller import GeometriesController
 from src.object.shape import Shape
@@ -12,15 +12,56 @@ class ShapeProps:
         GeometriesController.calculate_mesh(shape.geometries)
 
         shape_props = {}
-        center = shape.get_center()
+        center = shape.geometries.point_cloud.get_center()
 
-        verts = random.choices(list(shape.geometries.mesh.vertices), k=240)
+        n = 1000000
+        p = int(pow(n, 1.0 / 4.0))
+        vn = np.asarray(o3d.io.read_triangle_mesh(shape.geometries.path).sample_points_uniformly(number_of_points=n, seed=-1).points)
+        a3 = []
+        d1 = []
+        d2 = []
+        d3 = []
+        d4 = []
+        for i in range(p):
+            vi = vn[i]
+            for j in range(p):
+                vj = vn[p+j]
+                if all(vi == vj):
+                    continue
+                for k in range(p):
+                    vk = vn[2*p+k]
+                    if all(vk == vj) or all(vk == vi):
+                        continue
+                    for l in range(p):
+                        vl = vn[3*p+l]
+                        if all(vl == vk) or all(vl == vj) or all(vl == vi):
+                            continue
+                        a3.append(ShapeProps.calc_A3(vi, vj, vk))
+                        d1.append(ShapeProps.calc_D1(center, vi))
+                        d2.append(ShapeProps.calc_D2(vi, vj))
+                        d3.append(ShapeProps.calc_D3(vi, vj, vk))
+                        d4.append(ShapeProps.calc_D4(vi, vj, vk, vl))
 
-        shape_props["A3"] = ShapeProps.calc_A3(verts[:80], verts[80:160], verts[160:])
-        shape_props["D1"] = ShapeProps.calc_D1(center, verts)
-        shape_props["D2"] = ShapeProps.calc_D2(verts[:120], verts[120:])
-        shape_props["D3"] = ShapeProps.calc_D3(verts[:80], verts[80:160], verts[160:])
-        shape_props["D4"] = ShapeProps.calc_D4(verts[:60], verts[60:120], verts[120:180], verts[180:])
+
+        hist, bin_edges = np.histogram(np.array(a3), bins=int(np.sqrt(len(a3))))
+        hist = ShapeProps.normalize_hist(hist)
+        shape_props["A3"] = (hist, bin_edges)
+
+        hist, bin_edges = np.histogram(np.array(a3), bins=int(np.sqrt(3*len(d1))/2))
+        hist = ShapeProps.normalize_hist(hist)
+        shape_props["D1"] = (hist, bin_edges)
+
+        hist, bin_edges = np.histogram(np.array(a3), bins=int(np.sqrt(3*len(d2))))
+        hist = ShapeProps.normalize_hist(hist)
+        shape_props["D2"] = (hist, bin_edges)
+
+        hist, bin_edges = np.histogram(np.array(a3), bins=int(np.sqrt(len(d3))))
+        hist = ShapeProps.normalize_hist(hist)
+        shape_props["D3"] = (hist, bin_edges)
+
+        hist, bin_edges = np.histogram(np.array(a3), bins=int(np.sqrt(len(d4))))
+        hist = ShapeProps.normalize_hist(hist)
+        shape_props["D4"] = (hist, bin_edges)
 
     @staticmethod
     def calc_A3(v1, v2, v3):
@@ -45,3 +86,11 @@ class ShapeProps:
     @staticmethod
     def calc_D4(v1, v2, v3, v4):
         return np.cbrt(np.abs(np.dot(v1 - v4, np.cross(v2 - v4, v3 - v4))) / 6)
+
+    @staticmethod
+    def normalize_hist(hist):
+        hsum = np.sum(hist)
+        newhist = []
+        for hs in hist:
+            newhist.append(hs / hsum)
+        return np.asarray(newhist)
